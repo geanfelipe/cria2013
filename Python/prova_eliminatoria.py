66d8a7e8aa405dbfc6adb2e8eee856db02057a2f
Gean Felipe <geanfelipemoura@outlook.com>
2014-10-23 09:16:08 -0300

    adicionado matriz

--- Python/prova_eliminatoria.py
+++ Python/prova_eliminatoria.py
@@ -1,4 +1,33 @@
-﻿#São três salas idênticas com 360 lugares
+﻿from numpy import *
+
+"""
+version- python 2.7.3
+São três salas idênticas com 360 lugares
+"""
 
 
-def iniciar()
+#poltrona[sala,fila_letra,numero_poltrona]
+poltrona = arange(1260)  #matriz dos lugares //principal
+poltrona.shape = (3,21,20)
+
+print (poltrona)
+
+def iniciar():
+    """
+    Inicia todas as poltronas como vazia
+    """
+    global poltrona
+    for sala in range(3):
+        for fila_letra in range(21):
+            for numero_poltrona in range(20):
+                poltrona[sala,fila_letra,numero_poltrona]='0'
+
+print 
+iniciar()
+print (poltrona)
+
+sala  = int(input("numero da sala: "))
+fila_letra = str(input("letra da fileira: "))
+numero_poltrona = int(input("numero da poltrona: "))
+
+print (sala, fila_letra, numero_poltrona)
