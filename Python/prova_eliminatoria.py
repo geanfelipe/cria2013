339d16cfb53b94f459a4ec01ce4d2259a08cae3a
Gean Felipe <geanfelipemoura@outlook.com>
2014-10-26 17:40:15 -0300

    A) 90% concluido

--- Python/prova_eliminatoria.py
+++ Python/prova_eliminatoria.py
@@ -9,6 +9,8 @@ São três salas idênticas com 360 lugares
 #poltrona[sala,fila_letra,numero_poltrona]
 poltrona = arange(1260)  #matriz dos lugares //principal
 poltrona.shape = (3,21,20)
+matriz_fileira = "ABCDEFGHIJKLMNOPQRSTU"
+
 
 print (poltrona)
 
@@ -20,14 +22,59 @@ def iniciar():
     for sala in range(3):
         for fila_letra in range(21):
             for numero_poltrona in range(20):
-                poltrona[sala,fila_letra,numero_poltrona]='0'
+                poltrona[sala,fila_letra,numero_poltrona]=0
+
+def preencher(sala,fila_numero,numero_poltrona):
+    """
+    preencher os lugares do cinema
+    """
+    global poltrona
+    if poltrona[sala,fila_numero,numero_poltrona] == 0:
+        poltrona[sala,fila_numero,numero_poltrona]=1
+        return 1
+    else:
+        print "lugar já preenchido"
+        print "buscando um mais próximo"
+        for i in range(20):
+            if poltrona[sala,fila_numero,i]==0:
+                poltrona[sala,fila_numero,i]=1
+                print "preenchido na posicao %d da fileira %s " %(i+1,matriz_fileira[fila_numero])
+                return 1
+        print "fileira totalmente preenchida"
+        return 0
+        
+            
 
 print 
 iniciar()
-print (poltrona)
 
-sala  = int(input("numero da sala: "))
-fila_letra = str(input("letra da fileira: "))
-numero_poltrona = int(input("numero da poltrona: "))
+sala=1
+while 1:
+    print matriz_fileira
+    while 1:
+        sala  = int(input("numero da sala: "))
+        if not sala:
+            while 1:
+                try:
+                    print ": "
+                except:
+                    print "inválido"
+        if 0<sala<4: break
+    while 1:
+        fila_letra = str(raw_input("letra da fileira: "))
+        fila_letra = fila_letra.upper()
+        if matriz_fileira.count(fila_letra): break
+    while 1:
+        numero_poltrona = int(input("numero da poltrona: "))-1
+        if 0<=numero_poltrona<21 : break
 
-print (sala, fila_letra, numero_poltrona)
+    print
+
+    fila_numero = matriz_fileira.find(fila_letra)
+
+    print "antes: ",poltrona[sala,fila_numero,numero_poltrona]
+
+    retorno = preencher(sala,fila_numero,numero_poltrona)
+    print "depois: ",poltrona[sala,fila_numero,numero_poltrona]
+
+    print "retorno da funcao deu " ,retorno
