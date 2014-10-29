2bce0f15b55d2519c24c32bd4a6334f07c90814b
Gean Felipe <geanfelipemoura@outlook.com>
2014-10-29 03:17:06 -0300

    B) finalizado (sem bugs?)

--- prova_eliminatoria.py
+++ prova_eliminatoria.py
@@ -11,7 +11,6 @@ poltrona = arange(1260)  #matriz dos lugares //principal
 poltrona.shape = (3,21,20)
 matriz_fileira = "ABCDEFGHIJKLMNOPQRSTU"
 
-
 def iniciar():
     """
     Inicia todas as poltronas como vazia
@@ -34,7 +33,7 @@ def preencher(sala,fila_numero,numero_poltrona):
     else:
         print "lugar já preenchido"        
         for i in range(20):
-            if poltrona[sala,fila_numero,i]==0:
+            if poltrona[sala,fila_numero,i]==1:
                 poltrona[sala,fila_numero,i]=1
                 print "preenchido na posicao %d da fileira %s " %(i+1,matriz_fileira[fila_numero])
                 return 1
@@ -47,23 +46,91 @@ def relatorioDasSalas():
     relatório indicando qual a sala mais cheia do momento, qual a sala completamente lotada e qual a sala que mais possui poltronas
     sobrando nas três ultimas fileiras
     """
+    global poltrona
     sala1,sala2,sala3 = 0,0,0
     for fileira in range(21):
         for cadeiras in range(20):
             if poltrona[0,fileira,cadeiras]==0:
-                sala1+=sala1
+                sala1+=1
             if poltrona[1,fileira,cadeiras]==0:
-                sala2+=sala2
+                sala2+=1
             if poltrona[2,fileira,cadeiras]==0:
-                sala3+=sala3
-    if sala1==0: print 'sala 1 completamente lotada'
-    if sala2==0: print 'sala 2 completamente lotada'
-    if sala3==0: print 'sala 3 completamente lotada'
+                sala3+=1
+    print '|%d|%d|%d|' %(sala1,sala2,sala3)
+
     
+    #relatório indicando qual a sala mais cheia do momento, qual a sala completamente lotada
+    
+    if sala1==0:
+        print 'sala 1 completamente lotada'
+        if sala2<sala3:
+            print 'sala 2 mais cheia que sala 3 (%d)' %sala2
+        elif sala3<sala2:
+            print 'sala 3 mais cheia que sala 2 (%d)' %sala3
+        else: print 'sala 2 e 3 com o mesmo número de lotação (%d)' %sala2
+    
+    elif sala2==0:
+        print 'sala 2 completamente lotada'
+        if sala1<sala3:
+            print 'sala 1 mais cheia que sala 3 (%d)' %sala1
+        elif sala3<sala1:
+            print 'sala 3 mais cheia que sala 1 (%d)' %sala3
+        else: print 'sala 1 e 3 com o mesmo número de lotação (%d)' %sala3
+            
+        
+    elif sala3==0:
+        print 'sala 3 completamente lotada'
+        if sala2<sala1:
+            print 'sala 2 mais cheia que sala 1 (%d)' %sala2
+        elif sala1<sala2:
+            print 'sala 1 mais cheia que sala 2 (%d)' %sala1
+        else: print 'sala 1 e 2 com o mesmo número de lotação (%d)' %sala2
+    else:
+        if sala1<sala2<sala3 or sala1<sala3<sala2 or sala1<sala3==sala2:
+            print 'sala 1 mais cheia (%d)' %sala1
+        elif sala3<sala2<sala1 or sala3<sala1<sala2 or sala3<sala1==sala2:
+            print 'sala 3 mais cheia (%d)' %sala3
+        elif sala2<sala1<sala3 or sala2<sala3<sala1 or sala2<sala1==sala3:
+            print 'sala 2 mais cheia (%d)' %sala2
+        elif sala1==sala2<sala3:
+            print 'sala 1 e sala 2 mais cheia que sala 3 (%d)' %sala1
+        elif sala1==sala3<sala2:
+            print 'sala 1 e sala 3 mais cheia que sala 2 (%d)' %sala1
+        elif sala2==sala3<sala1:
+            print 'sala 2 e sala 3 mais cheia que sala 1 (%d)' %sala2
+        else :
+            print 'mesmo número de lotação nas salas (%d)' %sala1
+
+    #qual a sala que mais possui poltronas sobrando nas três ultimas fileiras
+    sala1,sala2,sala3 = 0,0,0
+    for fileira in range(3):
+        for lugar in range(20):
+            if poltrona[0,18+fileira,lugar]==0:
+                sala1 +=1
+            if poltrona[1,18+fileira,lugar]==0:
+                sala2 +=1
+            if poltrona[2,18+fileira,lugar]==0:
+                sala3 +=1
+    print '|%d|%d|%d|' %(sala1,sala2,sala3)
+    if sala1>sala2>sala3 or sala1>sala3>sala2 or sala1>sala3==sala2:
+        print 'sala 1 possui mais poltronas sobrando nas 3 ultimas fileiras (%d)' %sala1
+    elif sala3>sala2>sala1 or sala3>sala1>sala2 or sala3>sala1==sala2:
+        print 'sala 3 possui mais poltronas sobrando nas 3 ultimas fileiras (%d)' %sala3
+    elif sala2>sala1>sala3 or sala2>sala3>sala1 or sala2>sala1==sala3:
+        print 'sala 2 possui mais poltronas sobrando nas 3 ultimas fileiras (%d)' %sala2
+    elif sala1==sala2>sala3:
+        print 'sala 1 e 2 possui mais poltronas igualmente sobrando nas 3 ultimas fileiras (%d)' %sala2
+    elif sala1==sala3>sala2:
+        print 'sala 1 e 3 possui mais poltronas igualmente sobrando nas 3 ultimas fileiras (%d)' %sala1
+    elif sala2==sala3>sala1:
+        print 'sala 2 e 3 possui mais poltronas igualmente sobrando nas 3 ultimas fileiras (%d)' %sala2
+    else : print 'todas com mesma lotação nas 3 ultimas fileiras (%d)' %sala1
+
+
 
 iniciar()
 while 1:
-    print 
+    print
     print 'A) comprar ingressso'
     print 'B) relatório das salas'
     print 'C) imprimir salas'
@@ -78,11 +145,11 @@ while 1:
         while 1:
             try:
                 sala  = int(input("numero da sala: "))
-                if 0<sala<4: break
+                if 0<sala<4 or sala==10: break
                 else: print '#não existe'
             except:
                 print "#inválido"
-            
+        
         while 1:
             fila_letra = str(raw_input("letra da fileira: "))
             fila_letra = fila_letra.upper()
@@ -121,15 +188,16 @@ while 1:
 
     if opcao == 'B':
         relatorioDasSalas()
-
+        
     if opcao=='C':
         print poltrona
         print
     
     if opcao=='D':
+        global poltrona
         for i in range(21):
             for j in range(20):
-                poltrona[0,i,j]==1
+                poltrona[0,i,j]=1
     
     if opcao =='Q':
         print 'sair'
